aefe0c720fb22574bf9491669bf39a1a31f474a0
ryanmora23 <ryan.mora23@gmail.com>
2014-10-23 13:20:09 -0500

    ok, wrapping up install process and setting up git@github.com:phyllistran/HITCH.git

--- js/app.js
+++ js/app.js
@@ -17,3 +17,22 @@ function app(){
 
 }
 
+
+window.onload = app;
+
+// runs when the DOM is loaded
+function app(){
+
+    // load some scripts (uses promises :D)
+    loader.load(
+        {url: "./bower_components/jquery/dist/jquery.min.js"},
+        {url: "./bower_components/lodash/dist/lodash.min.js"},
+        {url: "./bower_components/pathjs/path.min.js"}
+    ).then(function(){
+        _.templateSettings.interpolate = /{([\s\S]+?)}/g;
+
+        // start app?
+    })
+
+}
+
